116b538cd3c5ee9ec93064a3f7962738dc528124
Munzambi-Miguel <35455479+Munzambi-Miguel@users.noreply.github.com>
2022-11-29 14:56:43 +0100

    set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { LoginComponent } from './page/auth/login/login.component';
+import { SingInComponent } from './page/auth/sing-in/sing-in.component';
+import { SingUpComponent } from './page/auth/sing-up/sing-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    LoginComponent,
+    SingInComponent,
+    SingUpComponent
   ],
   imports: [
     BrowserModule,
