691e688f3c290b5bf50e035ce60b7ecb6df030e4
Munzambi-Miguel <35455479+Munzambi-Miguel@users.noreply.github.com>
2023-02-24 00:48:13 +0100

    Open Stock Project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,13 @@ export const environment = {
   production: false,
   useEmulators: true,
   firebase: {
-    apiKey: "AIzaSyAkv4G9fiB9SqIiu04T3AhfI4Y0EdTz36Q",
-    authDomain: "global-tronic-storage-18502.firebaseapp.com",
-    projectId: "global-tronic-storage-18502",
-    storageBucket: "global-tronic-storage-18502.appspot.com",
-    messagingSenderId: "1034761865085",
-    appId: "1:1034761865085:web:6ba846af4187ff2c81ae14",
-    measurementId: "G-YL5X45WBM3",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
   }
 };
 
