733370c236ec1b0c848d6f9433f91082fd257841
Munzambi-Miguel <35455479+Munzambi-Miguel@users.noreply.github.com>
2023-02-27 13:51:40 +0100

    Sendo Clean

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   firebase: {
 
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: "",
+    apiKey: "AIzaSyAkv4G9fiB9SqIiu04T3AhfI4Y0EdTz36Q",
+    authDomain: "global-tronic-storage-18502.firebaseapp.com",
+    projectId: "global-tronic-storage-18502",
+    storageBucket: "global-tronic-storage-18502.appspot.com",
+    messagingSenderId: "1034761865085",
+    appId: "1:1034761865085:web:6ba846af4187ff2c81ae14",
+    measurementId: "G-YL5X45WBM3"
   },
   production: true
 };
